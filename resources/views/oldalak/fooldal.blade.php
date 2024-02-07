7f4bc08bd0287be12f815698a00d31d5c22c66e4
Emmatsoka <emmatsoka.qwq@gmail.com>
2024-02-07 07:56:07 +0100

    Főoldal létrehozása

---
+++ resources/views/oldalak/fooldal.blade.php
@@ -0,0 +1,17 @@
+
+<x-guest-layout>
+ 
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WESOC | Főoldal</title>
+</head>
+<body>
+
+    <h1>Készülőben</h1>
+
+</body>
+</html>
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('oldalak.fooldal');
 });
 
 Route::get('/dashboard', function () {
