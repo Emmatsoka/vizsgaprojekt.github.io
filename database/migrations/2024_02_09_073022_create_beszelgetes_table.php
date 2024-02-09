29bdf3d99b95964febf8a2c139d9dfcdba8e291c
Gabor2003 <gresugabor@gmail.com>
2024-02-09 08:38:36 +0100

    Módosítás/Bővités

--- database/migrations/2024_02_09_073022_create_beszelgetes_table.php
+++ database/migrations/2024_02_09_073022_create_beszelgetes_table.php
@@ -11,13 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('beszelgetes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('receiver_id')->references('id')->on('users');
+            $table->timestamp('last_time_message');
             $table->timestamps();
         });
     }
@@ -27,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('beszelgetes');
     }
 };
