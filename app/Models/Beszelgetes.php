65b22a09591415ff58c1ef588718ee72e7a205c6
Gabor2003 <gresugabor@gmail.com>
2024-02-09 09:58:10 +0100

    A modellek szerkesztése
    A kapcsolatok létrehozása

--- app/Models/Beszelgetes.php
+++ app/Models/Beszelgetes.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\Model;
 class Beszelgetes extends Model
 {
     use HasFactory;
+
+
+    protected $fillable=[
+        'sender_id',
+        'receiver_id',
+        'last_time_message',
+    ];
+
+    //relationships
+
+    public function message( )
+    {
+        return $this->hasMany(Uzenet::class);
+
+        # code...
+    }
+
+    public function user( )
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+        # code...
+    }
 }
--- app/Models/Uzenet.php
+++ app/Models/Uzenet.php
@@ -7,5 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Uzenet extends Model
 {
+   
     use HasFactory;
+
+    protected $fillable=[
+        'sender_id',
+        'receiver_id',
+        'last_time_message',
+        'conversation_id',
+        'read',
+        'body',
+    ];
+
+
+
+    
+    public function beszelget()
+    {
+        return $this->belongsTo(Beszelgetes::class);
+        # code...
+    }
+
+    public function user( )
+    {
+        return $this->belongsTo(User::class ,'sender_id');
+        # code...
+    }
+   
 }
