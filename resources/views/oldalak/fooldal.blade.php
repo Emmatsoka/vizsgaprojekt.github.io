144b92d02107290685d183471395caf0e52bd242
Emmatsoka <emmatsoka.qwq@gmail.com>
2024-02-17 23:26:06 +0100

    Főoldal készítése

--- resources/views/oldalak/fooldal.blade.php
+++ resources/views/oldalak/fooldal.blade.php
@@ -8,15 +8,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WESOC | Főoldal</title>
     <link rel="stylesheet" href="./styles/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./styles/webs.css">
+    <link rel="stylesheet" href="./styles/gorgo.css">
 </head>
 <body>
+    <header>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">WESOC</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center bold" id="navbarNav">
+          <ul class="navbar-nav ">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Támogatás</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Adatvédelem</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Elérhetőség</a>
+              </li>
+           
+          </ul>
+          
+        </div>
 
-    <h1>Készülőben </h1>
+            <a class=" gomb feherhtr float-right bold" href="#">Belépek</a>
+      
+      </nav>
 
+ 
+        <div id="headerszoveg">
+<h1>Egy oldal..</h1>
+<p>Ahol Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+<div id="headergombok">
+<button class="gomb feketehtr bold">Regisztráció</button><button class="gomb feherhtr bold">Megnyitás</button>
+</div>
+        </div>
+      </header>
+     <div id="tartalom">
 
-
-
-    
+     </div>
     <script src="./styles/bootstrap/js/bootstrap.min.js"></script>
 </body>
 
